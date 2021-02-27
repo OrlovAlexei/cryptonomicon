b9f1f4e83dcb6badd73843d3d01fef01ca39c9a9
Alexei <alexeiOrlov.dev@gmail.com>
2021-02-28 00:25:42 +0300

    feat(app): added pagination

--- src/App.vue
+++ src/App.vue
@@ -72,47 +72,64 @@
         </button>
       </section>
 
-      <template v-if="tickers.length">
-        <hr class="w-full border-t border-gray-600 my-4" />
-        <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <div
-            v-for="t in tickers"
-            :key="t.name"
-            class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-            :class="{
-              'border-4': sel === t,
-            }"
-            @click="select(t)"
-          >
-            <div class="px-4 py-5 sm:p-6 text-center">
-              <dt class="text-sm font-medium text-gray-500 truncate">{{ t.name }} - USD</dt>
-              <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ t.price }}
-              </dd>
-            </div>
-            <div class="w-full border-t border-gray-200"></div>
-            <button
-              @click.stop="handleDelete(t)"
-              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-            >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-                ></path></svg
-              >Удалить
-            </button>
+      <hr class="w-full border-t border-gray-600 my-4" />
+      <div>
+        <button
+          class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          v-if="page > 1"
+          @click="page = page - 1"
+        >
+          Назад
+        </button>
+        <button
+          class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          @click="page = page + 1"
+          v-if="hasNextPage"
+        >
+          Вперед
+        </button>
+        <div>Фильтр: <input v-model="filter" /></div>
+      </div>
+      <hr class="w-full border-t border-gray-600 my-4" />
+
+      <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        <div
+          v-for="t in filteredTicketsList()"
+          :key="t.name"
+          class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
+          :class="{
+            'border-4': sel === t,
+          }"
+          @click="select(t)"
+        >
+          <div class="px-4 py-5 sm:p-6 text-center">
+            <dt class="text-sm font-medium text-gray-500 truncate">{{ t.name }} - USD</dt>
+            <dd class="mt-1 text-3xl font-semibold text-gray-900">
+              {{ t.price }}
+            </dd>
           </div>
-        </dl>
-        <hr class="w-full border-t border-gray-600 my-4" />
-      </template>
+          <div class="w-full border-t border-gray-200"></div>
+          <button
+            @click.stop="handleDelete(t)"
+            class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
+          >
+            <svg
+              class="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="#718096"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clip-rule="evenodd"
+              ></path></svg
+            >Удалить
+          </button>
+        </div>
+      </dl>
+      <hr class="w-full border-t border-gray-600 my-4" />
 
       <section class="relative" v-if="sel">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">{{ sel.name }} - USD</h3>
@@ -179,9 +196,22 @@ export default defineComponent({
       showLoadingScreen: true as boolean,
       isTickerAllredyAdded: false as boolean,
       ticketsSuggestions: [] as string[],
+      filter: "" as string,
+      hasNextPage: true,
+      page: 1,
     };
   },
   created: function() {
+    const windowData = Object.fromEntries(new URL(window.location.toString()).searchParams.entries());
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = Number(windowData.page);
+    }
+
     this.loadTickersSummary();
     const tickerData = localStorage.getItem("crypto-list");
     if (tickerData) {
@@ -203,15 +233,19 @@ export default defineComponent({
       }
       this.addTickerByName(this.ticker);
     },
+    writeToListToLocalStorage(tickersToLocalStorage: Ticker[]) {
+      localStorage.setItem("crypto-list", JSON.stringify(tickersToLocalStorage));
+    },
     addTickerByName(tickerName: string) {
       const currentTicker = { name: tickerName, price: "-" };
       this.tickers.push(currentTicker);
 
-      localStorage.setItem("crypto-list", JSON.stringify(this.tickers));
+      this.writeToListToLocalStorage(this.tickers);
 
       this.subscribeToTickerChenges(currentTicker.name);
 
       this.ticker = "";
+      this.filter = "";
     },
     subscribeToTickerChenges(tickerName: string) {
       setInterval(async () => {
@@ -232,6 +266,7 @@ export default defineComponent({
     },
     handleDelete(tickerToRemove: Ticker) {
       this.tickers = this.tickers.filter(ticker => ticker !== tickerToRemove);
+      this.writeToListToLocalStorage(this.tickers);
     },
     normalizeGraph() {
       const max = Math.max(...this.graph);
@@ -260,6 +295,33 @@ export default defineComponent({
           return key;
         })
         .slice(0, 4);
+    },
+    filteredTicketsList(): Ticker[] {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filteredTickers = this.tickers.filter(t => t.name.includes(this.filter));
+
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
+  },
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`,
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`,
+      );
     },
   },
 });
